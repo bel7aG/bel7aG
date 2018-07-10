7ba7d4ac1f25ca55b433d40368b453b46d760a5b
Belhassen <Belhassen.Gharsallah@isetrades.com>
2018-07-10 22:19:43 +0200

    css GRID playground

--- src/playground/Grid.js
+++ src/playground/Grid.js
@@ -1,14 +1,17 @@
 import React from 'react';
 
 const GridPlayground = () => (
-  <div className="grid">
-    <div className="item item--1">1</div>
-    <div className="item item--2">2</div>
-    <div className="item item--3">3</div>
-    <div className="item item--4">4</div>
-    <div className="item item--5">5</div>
-    <div className="item item--6">6</div>
-  </div>
+  <section className="grid">
+    <h1>Grid Layout</h1>
+    <div className="grid-layout">
+      <div className="item item--1">1</div>
+      <div className="item item--2">2</div>
+      <div className="item item--3">3</div>
+      <div className="item item--4">4</div>
+      <div className="item item--5">5</div>
+      <div className="item item--6">6</div>
+    </div>
+  </section>
 );
 
 export default GridPlayground;
